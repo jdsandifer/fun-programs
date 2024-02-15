83a3217685159e3830a315a651075d92ea0cdeaa
J.D. Sandifer <sandifer.jd@gmail.com>
2024-02-15 08:34:21 -0800

    Refactor, add possibilities calc
    Also describes a way to label each
    position on the board that has some
    nice symmetry and patterns to it. It will
    probably come in handy for the actual
    solutions calculation logic.

--- the-bermuda-triangle-game.ts
+++ the-bermuda-triangle-game.ts
@@ -26,14 +26,6 @@ type BoardSide = Dot[];
  */
 type Piece = Dot[];
 
-/** How a piece fits in the puzzle */
-type Fit = {
-    /** Index in piece array */
-    piece: number;
-    /** Index of rotation of the piece */
-    fit: number;
-};
-
 // Color abbreviations
 const r: Dot = "Red";
 const y: Dot = "Yellow";
@@ -49,22 +41,22 @@ const rightSide: BoardSide = [b, r, g, k];
 const bottomSide: BoardSide = [g, g, w, g];
 
 const pieces: Piece[] = [
-    [r, k, g],
+    [r, k, g], // Piece 0
     [r, w, y],
     [y, w, g],
-    [b, k, w],
+    [b, k, w], // Piece 3
     [y, g, b],
     [b, w, w],
-    [r, k, g],
+    [r, k, g], // Piece 6
     [r, g, k],
     [g, k, k],
-    [y, g, k],
+    [y, g, k], // Piece 9
     [r, g, w],
     [y, w, g],
-    [b, k, w],
+    [b, k, w], // Piece 12
     [r, g, y],
     [b, b, w],
-    [y, k, b],
+    [y, k, b], // Piece 15
 ];
 
 /**
@@ -79,36 +71,105 @@ const pieces: Piece[] = [
  *     3.1 3.2 3.3 3.4 3.5
  * 4.1 4.2 4.3 4.4 4.5 4.6 4.7
  */
+type Row = number
+type Column = number
+type Position = `${Row}.${Column}`
 
-/** Determine the orientation (fit) for the piece to match the 2 dots given.
- * @returns A number 0-2 representing the fit or undefined
+/** 
+ * A representation of the already decided dots around a piece.
+ * Depending on the location in the puzzle, 1, 2, or 3 may be locked in.
  */
-const fitWithTwoSidesOrUndefined = (
-    firstDot: Dot,
-    secondDot: Dot,
-    piece: Piece
-): undefined | number => {
-    let fits: undefined | number = undefined;
-    [0, 1, 2].forEach((leftDotIndex) => {
-        const rightDotIndex = (leftDotIndex + 1) % 3;
-        if (
-            firstDot === piece[leftDotIndex] &&
-            secondDot === piece[rightDotIndex]
-        ) {
-            fits = leftDotIndex;
-        }
-    });
-    return fits;
+type DotSituation = [Dot] | [Dot, Dot] | [Dot, Dot, Dot]
+
+/** Index of rotation of the piece for a specific arrangement */
+type Rotation = 0 | 1 | 2;
+
+/** How a piece fits in the puzzle */
+type Fits = {
+    /** Index in piece array */
+    piece: number;
+    /** Index of rotation of the piece */
+    rotations: Rotation[];
 };
 
-const fitsInFirstSpotOrUndefined = (piece: Piece) =>
-    fitWithTwoSidesOrUndefined(leftSide[0], rightSide[0], piece);
 
-console.log("Starting solver");
 
-console.log("Pieces that fit in the first spot");
+
+/** Determine the orientations (fits) for the piece to match the dots given.
+ * @returns An array of numbers 0-2 representing the rotation(s) for a fit, 
+ * an empty array means it doesn't fit the constraints
+ */
+const fitsThatMatchTheConstraints = (
+    dots: DotSituation,
+    piece: Piece
+): Rotation[] => {
+    const rotationsToTry = [0, 1, 2]
+    const fitWorks = rotationsToTry.map((rotation) => {
+        let itFits = true
+        dots.forEach((dot, dotIndex) => {
+            const dotToCheckIndex = (rotation + dotIndex) % 3
+            const dotsMatch = dot === piece[dotToCheckIndex]
+            if (!dotsMatch) {
+                itFits = false;
+            }
+        })
+        return itFits
+    })
+    const fitsThatMatch = fitWorks.reduce<Rotation[]>((fitNumbers, doesFit, index) =>
+        doesFit ? fitNumbers.concat(index as Rotation) : fitNumbers, [])
+    return fitsThatMatch;
+};
+
+/** Determine the orientations (fits) for the piece to match the dot given.
+ * @returns An array of numbers 0-2 representing the orientation(s) for a fit
+ */
+const fitsThatMatchOneDot = (
+    dotToMatch: Dot,
+    piece: Piece
+): Rotation[] => {
+    return fitsThatMatchTheConstraints([dotToMatch], piece)
+};
+
+/** Determine the orientation (fit) for the piece to match the 2 dots given.
+ * @returns A number 0-2 representing the fit or undefined if it doesn't fit
+ */
+const fitsThatMatchTwoDots = (
+    firstDotToMatch: Dot,
+    secondDotToMatch: Dot,
+    piece: Piece
+): Rotation[] => {
+    return fitsThatMatchTheConstraints([firstDotToMatch, secondDotToMatch], piece)
+};
+
+const fitsThatMatchFirstSpot = (piece: Piece): Rotation[] =>
+    fitsThatMatchTwoDots(leftSide[0], rightSide[0], piece);
+
+
+
+
+console.log('Starting solver');
+console.log('There are 16 pieces and each has 3 possible orientations.')
+console.log('That means there are (16 * 3) * (15 * 3) * ... * (1 * 3) possible arrangements.')
+
+const possibilities =
+    (new Array(16))
+        .fill(undefined)
+        .map((_, index) => index + 1)
+        .map(numberOfPiecesLeft => numberOfPiecesLeft * 3)
+        .reduce((total, optionsForNextPiece) => total * BigInt(optionsForNextPiece), BigInt(1))
+
+console.log('That\'s %i possible ways to orient the pieces in the puzzle!', possibilities)
+
+console.log('Pieces that fit in the first spot');
 const firstSpotFits = pieces
-    .map((p, index) => [fitsInFirstSpotOrUndefined(p), index])
-    .filter(([fits]) => fits !== undefined)
-    .map(([fit, index]) => ({ piece: index, fit }));
+    .map((p, index) => [fitsThatMatchFirstSpot(p), index] as [Rotation[], number])
+    .filter(([rotations]) => rotations.length > 0)
+    .map(([rotations, index]) => ({ piece: index, fits: rotations }));
 console.log(firstSpotFits);
+
+console.log('Pieces that have at least one blue side')
+const piecesWithABlueDot = pieces
+    .map((p, index) => [fitsThatMatchOneDot(b, p), index] as [number[], number])
+    .filter(([rotations]) => rotations.length > 0)
+    .map(([rotations, index]) => ({ piece: index, fits: rotations }));
+console.log(piecesWithABlueDot);
